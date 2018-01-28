609a10cd9a5c8aa67de0f599a1c861303650ffbe
jasontwright <jasontwright@gmail.com>
2018-01-28 13:27:00 -0600

    init commit

---
+++ test.js
@@ -0,0 +1,11 @@
+// https://api.etherscan.io/api?module=account&action=balance&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae&tag=latest&apikey=YourApiKeyToken'
+
+var balUrl = '';
+
+var ethAdd = '0xa5fefbf4cb73a13ec329c1b815b436b03da26adb';
+var CBethAdd = '0xf5Df999008a808BCF03691701846c3ed0b9691B1';
+var CBbitAdd = '1CLceiwBxhBCiLNeXZbb2rx21hJjj6S3ux';
+var adaAdd = '';
+
+var apiKey = '';
+
